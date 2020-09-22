ff180972eaab294911acf3f4b0c090313641ea14
saha223311 <saha_97@bk.ru>
2020-09-22 20:21:15 +0300

    Realization of hm1.

--- hw1/js/index.js
+++ hw1/js/index.js
@@ -1,2 +1,13 @@
-const g = document.querySelector('g');
-const circle = document.querySelector('circle');
+const checkbox = document.getElementById('checkbox_id')
+const label = document.getElementById('label_id')
+const path = document.getElementById("path_id")
+
+checkbox.addEventListener('change', (event) => {
+  if (event.target.checked) {
+    label.innerHTML = "Happy"
+    path.setAttribute("d", "M 150 200 Q 225 300 300 200");
+  } else {
+    label.innerHTML = "Sad"
+    path.setAttribute("d", "M 150 250 Q 225 150 300 250");
+  }
+})
